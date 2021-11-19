cb0a61cbfb90f0381dde221d2258e98d79a3dd92
Felipe Pizarro <felipe.pizarro02@alumnos.ucn.cl>
2021-11-19 01:01:37 -0300

    Third Commit
    Sql inyection view house
    menu option
    arrienda method

--- test01/src/App.java
+++ test01/src/App.java
@@ -40,8 +40,14 @@ public class App {
         try{
             Connection conectate = DriverManager.getConnection(DB, user, password);
             JOptionPane.showMessageDialog(null, "Base de datos conectada con exito");
-            //inyeccion  sql con el arriendo nuevo 
-            
+            //inyeccion  sql con el arriendo nuevo
+            String query = "SELECT * FROM casa ;"; 
+            Statement st = conectate.createStatement();
+            ResultSet rs = st.executeQuery(query);
+
+            if (rs.next()){
+                System.out.println("id "+rs.getString(1)+" valor "+ rs.getString(2)+" Dueño "+ rs.getString(3));
+            }
         }catch(Exception error){
             JOptionPane.showMessageDialog(null, "intento de coneccion fallida revise las propiedades de coneion");
         }
@@ -50,15 +56,19 @@ public class App {
     public static boolean arrienda(String personArriendo,int dias, int año){
         if( dias <= 27 ){
             System.out.println("Ingreso de la casa dentro de lo permitido");
-
+            //llamar a la query para mostrar las casas disponibles
+            IngresarArriendo();
+            return true;
         }else{
             if (dias >= 28 ){
                 System.out.println("No puede escribir una incidencia de mas de 28 dias en un mismo año");
+                
             }
         }
+        return false;
         
         
-        return true;
+        
     }
 
 }
--- test01/src/db.sql
+++ test01/src/db.sql
@@ -1,21 +1,27 @@
-create database sunsetbandb;
-create table clientes (
-    id text not null primary key,
-    nombre text not null 
-);
-create table propietario (
-    id text not null primary key,
-    nombre text not null
-);
-create table casa (
-    numero int not null primary key,
-    valor_arrinedo int not null primary key,
-    id_propietario text ,
-    foreign key (id_propietario) references propietario(id)
-);
-create table arriendo (
-    id_cliente text,
-    id_dueño text,
-    foreign key (id_cliente) references clientes(id),
-    foreign key (id_dueño) references propietario(id)
-);
+    create database sunsetbandb;
+    create table clientes (
+        id text not null primary key,
+        nombre text not null 
+    );
+    create table propietario (
+        id text not null primary key,
+        nombre text not null
+    );
+    create table casa (
+        numero int not null primary key,
+        valor_arrinedo int not null ,
+        nombre_propietario text ,
+        foreign key (nombre_propietario) references propietario(nombre)
+    );
+    create table arriendo (
+        id_cliente text,
+        id_dueño text,
+        foreign key (id_cliente) references clientes(id),
+        foreign key (id_dueño) references propietario(id)
+    );
+    insert into propietario values ('19825202-6','Felipe Pizarro');
+    insert into casa values (1,300000,'Felipe Pizarro');
+    insert into casa values (2,360000,'Marcelo Guerra');
+    insert into casa values (3,370000,'Luis Flores');
+    insert into casa values (4,350000,'Diego Barros');
+    insert into casa values (5,320000,'Cristian Arqueros');
